3e682f512e91b04be3477fdeb268ba83ef1bb95b
DigoCast <diegocastilholorenceto@gmail.com>
2024-04-25 09:23:46 -0300

    adição do video e pagina sinopse

--- src/app.py
+++ src/app.py
@@ -5,5 +5,9 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
+@app.route('/sinopse')
+def sinopse():
+    return render_template('sinopse.html')
+
 if __name__ == '__main__':
     app.run(debug=True)
